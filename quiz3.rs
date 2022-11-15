64b6472c01d2b1d1025e293ce6d8439cbf73433e
cod1r <jasonho353@gmail.com>
2022-11-15 06:40:08 -0600

    first rustlings speedrun

---
+++ quiz3.rs
@@ -0,0 +1,66 @@
+// quiz3.rs
+// This quiz tests:
+// - Generics
+// - Traits
+// An imaginary magical school has a new report card generation system written in Rust!
+// Currently the system only supports creating report cards where the student's grade
+// is represented numerically (e.g. 1.0 -> 5.5).
+// However, the school also issues alphabetical grades (A+ -> F-) and needs
+// to be able to print both types of report card!
+
+// Make the necessary code changes in the struct ReportCard and the impl block
+// to support alphabetical report cards. Change the Grade in the second test to "A+"
+// to show that your changes allow alphabetical grades.
+
+// Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
+
+
+pub struct ReportCard {
+    pub grade: f32,
+    pub student_name: String,
+    pub student_age: u8,
+}
+
+impl ReportCard {
+    pub fn print_numeric(&self) -> String {
+        format!("{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.grade)
+    }
+    pub fn print_alpha(&self) -> String {
+        let alpha_grade = "A+";
+        format!("{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, alpha_grade)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn generate_numeric_report_card() {
+        let report_card = ReportCard {
+            grade: 2.1,
+            student_name: "Tom Wriggle".to_string(),
+            student_age: 12,
+        };
+        assert_eq!(
+            report_card.print_numeric(),
+            "Tom Wriggle (12) - achieved a grade of 2.1"
+        );
+    }
+
+    #[test]
+    fn generate_alphabetic_report_card() {
+        // TODO: Make sure to change the grade here after you finish the exercise.
+        let report_card = ReportCard {
+            grade: 2.1,
+            student_name: "Gary Plotter".to_string(),
+            student_age: 11,
+        };
+        assert_eq!(
+            report_card.print_alpha(),
+            "Gary Plotter (11) - achieved a grade of A+"
+        );
+    }
+}
---
+++ standard_library_types/iterators4.rs
@@ -0,0 +1,35 @@
+// iterators4.rs
+// Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
+
+
+pub fn factorial(num: u64) -> u64 {
+    if num == 0 {
+		1
+	} else {
+		(1..(num + 1)).fold(1, |n, c| n * c)
+	}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn factorial_of_0() {
+        assert_eq!(1, factorial(0));
+    }
+
+    #[test]
+    fn factorial_of_1() {
+        assert_eq!(1, factorial(1));
+    }
+    #[test]
+    fn factorial_of_2() {
+        assert_eq!(2, factorial(2));
+    }
+
+    #[test]
+    fn factorial_of_4() {
+        assert_eq!(24, factorial(4));
+    }
+}
